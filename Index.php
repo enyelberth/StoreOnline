15162b9574c7ad206384e9ef2a8ec1b19af1e113
Enyelberth Rodriguez <enyelberthrc22.z@gmail.com>
2023-08-12 22:11:23 -0400

    Probando commit

---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StoreOnline</title>
+</head>
+<body>
+    <h2>probando
+        
+    </h2>
+    /**    Modelo vista controlador*//
+
+</body>
+</html>
