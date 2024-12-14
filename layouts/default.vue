3cad7756e787d13bdc384e722c71661c26516026
enyelberth <enyelberthrc22.z@gmail.com>
2024-12-13 22:58:25 -0400

    feat: upda

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+
+<template>
+    <div class="bg-red-400 w-full h-full">
+    <div>
+        <Navbar :menuItems="menuData" />
+    </div>
+        <NuxtPage />
+        <Footer/>
+    </div>
+    
+</template>
+<script >
+export default {
+
+  data() {
+    return {
+      menuData: [
+        {
+          id: 1,
+          name: 'Inicio',
+          link: '#producto',
+          subItems: [
+            { id: '1-1', name: 'Sub Home 1', link: '#subhome1' },
+            { id: '1-2', name: 'Sub Home 2', link: '#subhome2' }
+          ]
+        },
+        {
+          id: 2,
+          name: 'Productos',
+          link: '#productos'
+        },
+        {
+          id: 3,
+          name: 'Login',
+          link: '#login'
+        }
+      ]
+    };
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-gray-400 h-screen">
+    <Carousel />
+    <h2>asdasd</h2>
+    
+  </div>
+</template>
+<script setup>
+
+</script>
